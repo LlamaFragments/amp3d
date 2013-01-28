09d34dc6104a8c91e46e92cc6ca1d547fd5e43d9
Walker Henderson <llama.fragments@gmail.com>
2013-01-28 05:40:17 -0600

    Super simple GLFW support.

--- Makefile
+++ Makefile
@@ -5,10 +5,9 @@ COMPILER_FLAGS = -c -g -O0 -Wall -Werror
 LINKER = g++
 
 amp3d.o : amp3d.cpp amp3d.h
-	$(COMPILER) $(COMPILER_FLAGS) amp3d.cpp
-
+	$(COMPILER) $(COMPILER_FLAGS)  amp3d.cpp 
 amp3d : amp3d.o
-	$(LINKER) amp3d.o -llua -o  amp3d
+	$(LINKER)  amp3d.o  -llua -lglfw -framework Cocoa -framework OpenGL -framework IOKit -o  amp3d
 
 clean :
 	-rm -f *.o amp3d
--- amp3d.cpp
+++ amp3d.cpp
@@ -7,7 +7,8 @@
  */
 
 #include "amp3d.h"
-
+#include <GL/glfw.h>
+#include <iostream>
 extern "C"
 {
     #include "lua.h"
@@ -15,26 +16,54 @@ extern "C"
     #include "lauxlib.h"
 }
 
+using namespace std;
 
-/* the Lua interpreter */
-
+//Lua interpreter
 lua_State* L;
 
 int main ( int argc, char *argv[] )
 {
-	/* initialize Lua */
+    //Init GLFW
+    glfwInit();
+    //glfwOpenWindowHint(GLFW_OPENGL_PROFILE, 0);
+    int result = glfwOpenWindow(0, 0, 0, 0, 0, 0, 0, 0, GLFW_WINDOW);
+    if (result == GL_TRUE)
+    {
+        cout << "worked!" << endl;
+    }
+    else
+    {
+        cout << "failed" << endl;
+    }
+    int running = GL_TRUE;
+    // Main loop
+    while( running )
+    {
+        // OpenGL rendering goes here...
+        glClear( GL_COLOR_BUFFER_BIT );
+        // Swap front and back rendering buffers
+        glfwSwapBuffers();
+        // Check if ESC key was pressed or window was closed
+        running = !glfwGetKey( GLFW_KEY_ESC ) &&
+        glfwGetWindowParam( GLFW_OPENED );
+    }
+    
+    //glfwCloseWindow();
+    glfwTerminate();
+    
+	//Initialize Lua
 	L = luaL_newstate();
     
-	/* load various Lua libraries */
-
+	//Load various Lua libraries
 	luaL_openlibs(L);
     luaopen_table(L);
     luaopen_io(L);
     luaopen_string(L);
     luaopen_math(L);
-    
-	/* cleanup Lua */
-
+    //Run the lua script
+	luaL_dofile(L, "lua_script.lua");
+	
+    //Clean up Lua
 	lua_close(L);
     
 	return 0;
